fef29c09d507a090a3ae322ad4a563aca1da4682
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2024-01-17 09:58:28 +0100

    Add files via upload

---
+++ contactType.ts
@@ -0,0 +1,10 @@
+export type Contact = {
+  firstname: string,
+  lastname: string,
+  email: string,
+  personalnumber: string,
+  address: string,
+  zipCode: number,
+  city: string,
+  country: string
+}
